7963bc363a4fe37ab74c27183f327a78d0553b78
Abrar hussain dar <63048954+Abrarhussaindar@users.noreply.github.com>
2024-04-05 18:35:30 +0530

    adh

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -887,9 +887,7 @@ function Form() {
                                         <option value="8">8</option>
                                         <option value="9">9</option>
                                     </select>
-                                    <div>
-
-                                    </div>
+                                    
                                     <select className="adhVal" value={adVal4} onChange={(e) => setAdVal4(e.target.value)}>
                                         <option value=""></option>
                                         <option value="0">0</option>
@@ -942,9 +940,7 @@ function Form() {
                                         <option value="8">8</option>
                                         <option value="9">9</option>
                                     </select>
-                                    <div>
-
-                                    </div>
+                                
                                     <select className="adhVal" value={adVal8} onChange={(e) => setAdVal8(e.target.value)}>
                                         <option value=""></option>
                                         <option value="0">0</option>
